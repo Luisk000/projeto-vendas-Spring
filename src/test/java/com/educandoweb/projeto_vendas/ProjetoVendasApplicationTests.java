84375860221b3445747422f661b4db6f2e83458b
luis-oliveira <luis.holiveira@ext.evertecinc.com.br>
2026-05-26 11:58:47 -0300

    Projeto criado

---
+++ src/test/java/com/educandoweb/projeto_vendas/ProjetoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projeto_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
