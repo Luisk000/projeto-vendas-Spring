cf61ebf6519a2639e804c39e0299884f73195c7a
luis-oliveira <luis.holiveira+sinqia@ext.evertecinc.com.br>
2026-05-28 08:34:12 -0300

    Entidade Order adicionada

--- src/main/java/com/educandoweb/projeto_vendas/entities/User.java
+++ src/main/java/com/educandoweb/projeto_vendas/entities/User.java
@@ -3,6 +3,8 @@ package com.educandoweb.projeto_vendas.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    //Um usuário para muitas orders
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -47,6 +53,10 @@ public class User implements Serializable {
 
     public String getPhone() {
         return phone;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public void setPassword(String password) {
