407a3297ddb14ea87c807a60e4dccbfe3a03b1dc
luis-oliveira <luis.holiveira+sinqia@ext.evertecinc.com.br>
2026-06-08 16:58:37 -0300

    Exception databaseError

--- src/main/java/com/educandoweb/projeto_vendas/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/projeto_vendas/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.educandoweb.projeto_vendas.resources.exceptions;
 
+import com.educandoweb.projeto_vendas.services.exceptions.DatabaseException;
 import com.educandoweb.projeto_vendas.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,21 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(
+                Instant.now(),
+                status.value(),
+                error,
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return  ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(
                 Instant.now(),
                 status.value(),
--- src/main/java/com/educandoweb/projeto_vendas/services/UserService.java
+++ src/main/java/com/educandoweb/projeto_vendas/services/UserService.java
@@ -2,8 +2,11 @@ package com.educandoweb.projeto_vendas.services;
 
 import com.educandoweb.projeto_vendas.entities.User;
 import com.educandoweb.projeto_vendas.repositories.UserRepository;
+import com.educandoweb.projeto_vendas.services.exceptions.DatabaseException;
 import com.educandoweb.projeto_vendas.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.lang.module.ResolutionException;
@@ -32,7 +35,15 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
